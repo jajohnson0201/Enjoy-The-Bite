d31cbffdf6e002b8624944fead5f33433b9bf106
joe <jajohnson0201@gmail.com>
2023-02-06 19:36:22 -0600

    made files visible

---
+++ models/User.js
@@ -0,0 +1,11 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class  extends Model {}
+
+.init(
+    {
+        id: {}
+    }
+)
