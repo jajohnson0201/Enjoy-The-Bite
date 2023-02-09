eb6a85a8233097d8c38a4a0a7f301035f0ee343e
abasheryassin <abasheryassin@gmail.com>
2023-02-08 20:30:18 -0600

    add comments to code

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// Creats a new account 
 router.post('/', async (req, res) => {
     try {
         const userData = await User.create(req.bdoy);
@@ -16,8 +17,10 @@ router.post('/', async (req, res) => {
     }
 });
 
+//logs user in if the already have an account
 router.post('/login', async (req, res) => {
     try {
+        //checks to see if email exisits in the db
         const userData = await User.findOne({
             where: {
                 email: req.body.email
@@ -29,6 +32,7 @@ router.post('/login', async (req, res) => {
             return;
         }
 
+        //checks to see if password matches the the corresponding emails password
         const vaildPassword = await userData.checkPassword(req.body.password);
 
         if (!vaildPassword) {
@@ -36,6 +40,7 @@ router.post('/login', async (req, res) => {
             return;
         }
 
+        //signs user into a session.
         req.session.save(() => {
             req.session.user_id = userData.id;
             req.session.logged_in = true;
@@ -49,7 +54,7 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/logout', (req, res) => {
-    
+
     if(req.session.logged_in) {
         req.session.destroy(() => {
             res.status(204).end();
