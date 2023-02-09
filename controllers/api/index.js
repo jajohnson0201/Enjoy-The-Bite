5ae9a1fc6e6f5a95f6e7e6dd19d2bc9a23c4e7f2
abasheryassin <abasheryassin@gmail.com>
2023-02-08 21:26:14 -0600

    api fix

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRouter = require('./userRoutes');
 
-router.use('users', userRouter);
+router.use('/users', userRouter);
 
 module.exports = router;
