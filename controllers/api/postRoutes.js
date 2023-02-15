870f69ed1f2ca331394121ba9f21126491b0aef3
zaki ahmed <zakiahmed@zakis-MacBook-Pro.local>
2023-02-15 17:57:06 -0600

    finalizing postRoutes added a get router

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,29 +3,30 @@ const { postObject } = require('../../models');
 const authurizeLog = require("../../views/utils/auth");
 
 //creating a new post with user id 
-router.post('/', authurizeLog , async (req, res) => {
+router.post('/' , async (req, res) => {
     try {
       const newPost = await postObject.create({
         ...req.body,
-        user_id: req.session.user_id,
+        // user_id: req.session.user_id,
   
       });
       res.status(200).json(newPost);
     } catch (err) {
+      console.log(err);
       res.status(400).json(err);
     }
   });
   //making a delete post wuth the id and users id
   router.delete('/:id', authurizeLog, async (req, res) => {
     try {
-      const postData = await postObjec.destroy({
+      const postData = await postObject.destroy({
         where: {
           id: req.params.id,
           user_id: req.session.user_id,
         },
       });
   //404 error and a message of no data is found
-      if (!posttData) {
+      if (!postData) {
         res.status(404).json({ message: 'No data found with this id!' });
         return;
       }
@@ -34,6 +35,16 @@ router.post('/', authurizeLog , async (req, res) => {
     } catch (err) {
       res.status(500).json(err);
     }
+});
+
+router.get('/',  async (req, res) => {
+  try {
+    const postData = await postObject.findAll();
+    res.status(200).json(postData);
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err);
+  }
 });
   
   module.exports = router;
