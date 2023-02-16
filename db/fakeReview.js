2cd309fd18d2fea94cfa2c385ea599153b513f12
joe <jajohnson0201@gmail.com>
2023-02-15 20:25:23 -0600

    added new user and review to etb_db

--- db/fakeReview.js
+++ db/fakeReview.js
@@ -1,11 +1,68 @@
 const faker = require("@faker-js/faker");
 faker.setLocale('en_US');
-const randomName = faker.name.findName();
-const randomPhoto = faker.image.food();
-const randNoun = faker.word.noun();
-const adjective = faker.word.adjective();
-let randReview;
-function createRandomReview (){
-let randReview = ``;
+const Review = require("../models/Review");
+const User = require("../models/User");
+const mysql = require("mysql2");
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        // MySQL password
+        password: 'chicken',
+        database: 'etb_db'
+    },
+);
+let firstName = faker.name.firstName();
+let lastName = faker.name.lastName();
+let photo = faker.image.food();
+let noun = faker.word.noun();
+let adverb = faker.word.adverb();
+let adjective = faker.word.adjective();
+let interjection = faker.work.interjection();
+let randRating = faker.datatype.number({ max: 5 });
+let rEmail = faker.internet.email();
+let rPassword = faker.internet.password();
+let newUser;
+let newReview;
+
+async function createRandomUser () {
+    let user = await new User.init({
+        userName: {firstNamelastName},
+        firstName: {firstName},
+        lastName: {lastName},
+        email: {rEmail},
+        password: {rPassword}
+    });
+    newUser = [firstNamelastName,firstName,lastName,email,password];
+    return user
+}
+
+async function createRandomReview () {
+let randReview = await
+`Reviewing: ${adjective} ${noun}
+By: ${firstName} ${lastName}
+${photo}
+The food was quite ${adverb} ${adjective}!
+${interjection}
+${randRating}/5!!!
+`;
 return randReview
 }
+
+async function createRandomReview () {
+let PO = await new Review.init({
+    reviewBody: {createRandomReview},
+    foodRating: {randRating},
+    locationRating: {randRating},
+    overallRating: {randRating},
+});
+newReview = [createRandomReview, randRating, randRating, randRating];
+return PO;
+}
+
+db.query(`USE etb_db`);
+db.query(`INSERT INTO user(userName,firstName,lastName,email,password) VALUES(?,?,?,?,?)`,
+newUser);
+db.query(`INSERT INTO review(reviewBody,foodRating,locationRating,overallRating) VALUES(?,?,?,?)`,
+newReview);
+
--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,22 @@
 DROP DATABASE IF EXISTS etb_db;
 CREATE DATABASE etb_db;
+
+CREATE TABLE user (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    userName VARCHAR(30),
+    firstName VARCHAR(30),
+    lastName VARCHAR(30),
+    email,
+    password
+);
+CREATE TABLE review (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    reviewBody VARCHAR(300),
+    foodRating INT,
+    locationRating INT,
+    overallRating INT,
+    userID INT,
+    FOREIGN KEY (userID)
+    REFERENCES user(id)
+    ON DELETE SET NULL
+);
