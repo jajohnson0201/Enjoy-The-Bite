0d678f1992fdad9899e30d3355c91e05f3481ffc
abasheryassin <abasheryassin@gmail.com>
2023-02-16 17:33:33 -0600

    final touches

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRouter = require('./userRoutes');
-const postRoutes = require('./postObjectRoutes');
+const postRoutes = require('./postRoutes');
 
 
 router.use('/users', userRouter);
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -7,7 +7,7 @@ router.post('/' , async (req, res) => {
     try {
       const newPost = await Review.create({
         ...req.body,
-        // user_id: req.session.user_id,
+        user_id: req.session.user_id,
   
       });
       res.status(200).json(newPost);
